7a764d9b31b554e493249c26ec5c03e4a89110d0
Catherine Sharon <catherine.sharon@cerner.com>
2017-02-19 16:58:04 +0530

    Java : Student Records

---
+++ src/studrec/SemesterList.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studrec;
+
+import java.io.*;
+import java.util.*;
+/**
+ *@author  Jeshurun Ray Flores
+ *@version 1.0
+ *@see     Hashtable
+ */
+public class SemesterList extends Hashtable implements Serializable
+{
+  /**
+   *Sorts the Semester List according to Semester Code in ascending order
+   *@return Vector containing sorted elements.
+   */
+  public Vector sort(){
+    Set sKeys = this.keySet();
+    Vector temp = new Vector(sKeys);
+    Collections.sort(temp);
+    
+    Vector sorted = new Vector();
+    
+    for(int a=0;a<temp.size();a++){
+      sorted.add(this.get(temp.elementAt(a)));
+    }
+    
+    return sorted;
+  }
+}//class
